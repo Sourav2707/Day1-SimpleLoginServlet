2768ddbeb9a6e8f2f518e2b70b988caebe31d544
Sourav2707 <smartsourav33@gmail.com>
2023-06-02 22:12:18 +0530

    UC1 creating my first servlet

---
+++ src/main/java/com/bridelabz/FirstServlet.java
@@ -0,0 +1,19 @@
+package com.bridelabz;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello world, Sourav's first servlet!<h3>");
+        out.close();
+    }
+}
